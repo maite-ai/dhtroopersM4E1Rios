6503ee4046f5924287b51e463fd9503bfd93d862
peirios <mariafernandarios89@gmail.com>
2021-04-02 00:24:12 -0300

    Se agrega archivos login y register. Corrección Home inconos

--- app.js
+++ app.js
@@ -8,4 +8,8 @@ app.use(express.static(publicPatch));
 
 app.get("/", (req, res) => { res.sendFile(path.join(__dirname, "/views/home.html")) });
 
+app.get("/register", (req, res) => { res.sendFile(path.join(__dirname, "/views/register.html")) });
+
+app.get("/login", (req, res) => { res.sendFile(path.join(__dirname, "/views/login.html")) });
+
 app.listen(port, () => console.log("Escuchando en puerto " + port));
